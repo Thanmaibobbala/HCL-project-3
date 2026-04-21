55da95a4f49bda0f9fd126659ed4a910a97a541d
Thanmaibobbala <thanmai_bobbala@srmap.edu.in>
2026-04-21 14:38:36 +0530

    Update LoginPage.java

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import base.BasePage;
 
 import java.time.Duration;
-
 public class LoginPage extends BasePage {
 
     public LoginPage(WebDriver driver) {
